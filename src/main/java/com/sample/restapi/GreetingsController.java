4d6aad353cea75984e963729df37d4922d272b05
Miztli Melgoza <emelgoza@tacitknowledge.com>
2020-12-01 11:29:21 -0600

    Add greetings controller

--- src/main/java/com/sample/restapi/GreetingsController.java
+++ src/main/java/com/sample/restapi/GreetingsController.java
@@ -1,5 +1,7 @@
 package com.sample.restapi;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingsController
 {
     @GetMapping
-    public String greetings(@RequestParam final String name) {
-        return "Hello " + name + "!";
+    public Map<String, String> greetings(@RequestParam final String name) {
+        return Map.of("greetings", "Hello " + name + "!");
     }
 }
